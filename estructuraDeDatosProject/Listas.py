764c77502b5eb78126362ce3b221c9e566599306
Santiago Ruano <ivansantiagoruanoruiz3@gmail.com>
2024-10-24 17:47:36 -0500

    se suber los archivos

---
+++ estructuraDeDatosProject/Listas.py
@@ -0,0 +1,11 @@
+#Liatas
+lista = [] #Definir una lista vacia
+print(lista)
+lista1 = [1,2,3,4,5, "hola",4.5]#Una lista heterogenea
+print(lista1)
+
+#Enlazar las listas
+lista2 = [0,1,2,3]
+lista3 = ["A","B","C"]
+lista4 = [lista2, lista3]
+print(lista4)
